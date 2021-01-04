c7df2b45d59c7ef1b122f6a165a93c6eb1b6663e
hsingh <hsingh@osidigital.com>
2021-01-04 17:35:20 +0530

    Code refactor

--- public/scripts/questions.js
+++ public/scripts/questions.js
@@ -1,28 +1,30 @@
-var db = firebase.firestore();
-var user = firebase.auth().currentUser;
-var functions = firebase.functions();
-let uid;
+const container = $('.container-fluid');
+let qList = null;
+let gameid;
 
-firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-        console.log('User is NOT NULL ::' + user.uid + "; displayname ::" + user.displayName);
-        $('#loggedInUser').text(user.displayName);
-        uid = user.uid;
-        hideButtons(true);
-    } else {
-        console.log('User is NULL');
-        hideButtons(false);
-    }
-  });
-  
 
-function createNode(element) {
-	return document.createElement(element);
-}
+/**
+ * Called when user is logged in
+ */
+successLogin = function() {
+    console.log('Inside successLogin');
+};
+/**
+ * Called when user is NOT logged in
+ */
+failureLogin = function() {
+    console.log('Inside failureLogin');
+};
 
+
+/**
+ * Creates each UI row while iterating through question list JSON
+ * @param {*} ques - Question text
+ * @param {*} answ - Answer text
+ * @param {*} index - The counter
+ * @param {*} container - Parent element to which this row is added
+ */
 function createQuestionRow(ques, answ, index, container) {
-    // let ques = `${question['Question']}`;
-    // let answ = `${question['Answer']}`;
     let rowsno = index + 1 + '.';
     let rowquestion = ques;
     let rowanswer = answ;
@@ -32,8 +34,6 @@ function createQuestionRow(ques, answ, index, container) {
     let snodiv = createNode('div');
     let quesdiv = createNode('div');
     let answdiv = createNode('div');
-    // console.log(`${question['Question']}`);
-    // let row = container.append('div');
     
     $(snodiv).addClass('col-lg-1 col-sm-12');
     $(quesdiv).addClass('col-lg-7 col-md-auto col-sm-12');
@@ -41,11 +41,7 @@ function createQuestionRow(ques, answ, index, container) {
     $(snodiv).html(rowsno);
     $(quesdiv).html(rowquestion);
     $(answdiv).html(rowanswer);
-    // console.log(snodiv);
-    // console.log(answdiv);
     row.append(snodiv, quesdiv, answdiv);
-    // row.append(quesdiv);
-    // row.append(answdiv);
     $(row).addClass('line row');
     
     container.append(row);
@@ -60,79 +56,70 @@ function createQuestionRow(ques, answ, index, container) {
 
 
 
-// Read Data
-const container = $('.container-fluid');
-let qList = null;
-let gameid;
-
+/**
+ * First method that initiates data fetch from Firestore / Cache
+ */
 function init() {
-    // sessionStorage.clear();
     // clearStorage();
-    db.collection("settings").doc("currgame").get()
-    .then((doc) => {
-        gameid = doc.data().gameid;
-        if (getFromStorage('gameid') != null  
-                &&  doc.data().gameid == getFromStorage('gameid')  
-                &&  doc.data().queschanged.seconds == getFromStorage('queschanged')) {
-            // Questions not changed, use the data from cache
-            console.log("Picking data from Cache");
-            qList = JSON.parse(getFromStorage("qlist"));
-            iterateQuestions(qList);
-        }
-        else {
-            // Clear all storage includeing storate of ticket and other pages
-            clearStorage();
-
-            // Picking data from game questions - gameques/<gameid>/questions
-            db.collection("gameques").doc(gameid).get()
-            .then((doc) => {
-                console.log("Picking data from firestore");
-                qList = doc.data();
-                addToStorage("qlist", JSON.stringify(qList));
-                qList = JSON.parse(getFromStorage("qlist"));
-                iterateQuestions(qList);
-            });
-        }
-        addToStorage('gameid', doc.data().gameid);
-        addToStorage('queschanged', doc.data().queschanged.seconds);
-        addToStorage('gamedatetime', doc.data().gamedatetime.seconds);
-
-        $('.gamedate').text( new Date(doc.data().gamedatetime.seconds*1000) );
-    });
+    getFSSettingsData(successCurrGameFetch, null);
 }
 
-function iterateQuestions(qList) {
-    let index = 0;
-    // var answers = '';
-    Object.keys(qList).forEach((qdockey) => {
-        let qdoc = qList[qdockey];
-        // if (answers.length > 0) answers += '#';
-        // console.log(`${qdoc} => ${qdoc.question} => ${qdoc.answer}`);
-        createQuestionRow(qdoc.question, qdoc.answer, index, container);
-        // answers += qdoc.answer;
-        index++
-    });
-    // console.log(answers);
-}
-
-
-function hideButtons(loggedIn) {
-    if (!loggedIn) {
-        $('#btnSignup').hide();
-        $('#btnLogin').show();
-        $('#btnTicket').hide();
-        $('#btnLogout').hide();
+/**
+ * Called when Current Game Settings data is fetched from Firestore
+ * @param {*} doc - JSON Data - current game settings
+ */
+function successCurrGameFetch(doc) {
+    gameid = doc.data().gameid;
+    if (getFromStorage('gameid') != null  
+            &&  doc.data().gameid == getFromStorage('gameid')  
+            &&  doc.data().queschanged.seconds == getFromStorage('queschanged')) {
+        // Questions not changed, use the data from cache
+        console.log("Picking data from Cache");
+        qList = JSON.parse(getFromStorage("qlist"));
+        iterateQuestions(qList);
     }
     else {
-        $('#btnTicket').show();
-        $('#btnLogout').show();
-        $('#btnSignup').hide();
-        $('#btnLogin').hide();
+        // Clear all storage including storage of ticket and other pages
+        clearStorage();
+
+        // Picking data from game questions - gameques/<gameid>/questions
+        getFSCurrGameQuestions(gameid, successQuestionListFetch, null);
     }
+    addToStorage('gameid', doc.data().gameid);
+    addToStorage('queschanged', doc.data().queschanged.seconds);
+    addToStorage('gamedatetime', doc.data().gamedatetime.seconds);
+
+    $('.gamedate').text( new Date(doc.data().gamedatetime.seconds*1000) );
 }
 
-init();
+/**
+ * Called when question list for current game is fetched from Firestore
+ * @param {*} doc - JSON Data - question list
+ */
+function successQuestionListFetch(doc) {
+    console.log("Picked data from firestore");
+    qList = doc.data();
+    addToStorage("qlist", JSON.stringify(qList));
+    qList = JSON.parse(getFromStorage("qlist"));
+    iterateQuestions(qList);
+}
 
+/**
+ * Iterate JSON data to create UI
+ * @param {*} qList - JSON data from Firestore or Cache
+ */
+function iterateQuestions(qList) {
+    let index = 0;
+    Object.keys(qList).forEach((qdockey) => {
+        let qdoc = qList[qdockey];
+        createQuestionRow(qdoc.question, qdoc.answer, index, container);
+        index++
+    });
+}
+
+/**
+ * Handler for Signout button
+ */
 function signout() {
     firebase.auth().signOut().then(function() {
         // Sign-out successful.
@@ -142,7 +129,7 @@ function signout() {
     });
 }
 
-function getTicket() {
+/* function getTicket() {
     let gameId = getFromStorage('gameid');
     db.collection("tickets").doc(gameId + "_" + uid).get()
     .then((doc) => {
@@ -152,9 +139,13 @@ function getTicket() {
         console.error("Error getting ticket: ", error);
     });
     return null;
-}
+} */
 
 
+/**
+ * Handler for Ticket button
+ * @param {*} e - event
+ */
 function generateTicket(e) {
     e.preventDefault();
     console.log( getFromStorage('gamedatetime') );
@@ -173,11 +164,10 @@ function generateTicket(e) {
     }
 }
 
-$(function onDocReady() {
-	console.log('Inside onDocReady');
-    $('#btnLogout').click(signout);
-    $('#btnTicket').click(generateTicket);
-
+/**
+ * Displays Sharing button on UI. Picks data from share.html; also sets the href for buttons
+ */
+function loadSharingButtons() {
     $('.sharewrapper').load('pagelets/share.html', function() {
         let currURL = $(location).attr('href');
         console.log(currURL);
@@ -187,4 +177,17 @@ $(function onDocReady() {
         $('.emailshare').attr('href', 'mailto:?subject=Learn about Sikh History in a fun way: Sikh History Tambola.&body=' + currURL);
         $('.whatsappshare').attr('href', 'whatsapp://send?text=Learn about Sikh History in a fun way: Sikh History Tambola. ' + currURL);
     });
+}
+
+/**
+ * On page load function
+ */
+$(function onDocReady() {
+	console.log('Inside onDocReady');
+    $('#btnLogout').click(signout);
+    $('#btnTicket').click(generateTicket);
+    loadSharingButtons();
 });
+
+checkLogin(firebase.auth(), successLogin, failureLogin);
+init();
--- public/scripts/storage.js
+++ public/scripts/storage.js
@@ -1,3 +1,8 @@
+var db = firebase.firestore();
+
+/* ************************************************** */
+/* ******************** CACHE *********************** */
+/* ************************************************** */
 function addToStorage(key, value) {
     sessionStorage.setItem(key, value);
 }
@@ -18,10 +23,76 @@ function removeFromStorage(key) {
     sessionStorage.removeItem(key);
 }
 
+
+
+/* ************************************************** */
+/* ******************* LOGGING ********************** */
+/* ************************************************** */
+function logMessage(msg) {
+	// console.log(msg);
+}
+
+/* ************************************************** */
+/* ******************** LOGIN *********************** */
+/* ************************************************** */
+function checkLogin(auth, successFunction, failureFunction) {
+    console.log('Inside checkLogin');
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+            console.log('User is NOT NULL ::' + user.uid + "; displayname ::" + user.displayName);
+            $('#loggedInUser').text(user.displayName);
+            uid = user.uid;
+            hideHeaderButtons(true);
+            if (successFunction !== null  &&  successFunction !== undefined) successFunction();
+        } else {
+            console.log('User is NULL');
+            hideHeaderButtons(false);
+            if (failureFunction !== null  &&  failureFunction !== undefined) failureFunction();
+        }
+    });
+}
+
+/* ************************************************** */
+/* ****************** COMMON UI ********************* */
+/* ************************************************** */
+function hideHeaderButtons(loggedIn) {
+    if (!loggedIn) {
+        $('#btnSignup').hide();
+        $('#btnLogin').show();
+        $('#btnTicket').hide();
+        $('#btnLogout').hide();
+    }
+    else {
+        $('#btnTicket').show();
+        $('#btnLogout').show();
+        $('#btnSignup').hide();
+        $('#btnLogin').hide();
+    }
+}
+
 function createNode(element) {
 	return document.createElement(element);
 }
 
-function logMessage(msg) {
-	// console.log(msg);
+
+/* ************************************************** */
+/* ****************** FIRESTORE ********************* */
+/* ************************************************** */
+function getFirestoreData(collName, docName, success, failure) {
+    db.collection(collName).doc(docName).get()
+    .then((doc) => {
+        console.log('Calling success');
+        success(doc);
+    })
+    .catch((error) => {
+        if (failure !== null  &&  failure !== undefined) failure(error);
+    });
+}
+
+function getFSSettingsData(success, failure) {
+    getFirestoreData("settings", "currgame", success, failure);
+}
+
+function getFSCurrGameQuestions(gameId, success, failure) {
+    getFirestoreData("gameques", gameId, success, failure);
 }
