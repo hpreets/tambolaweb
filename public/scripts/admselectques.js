9245d0f535781a01e1d6b3cb7ba33c813fed5b9d
Harpreet Singh <harpreet.singh@finastra.com>
2021-12-26 15:32:56 +0530

    create new gameques from existing gameques

--- public/scripts/admselectques.js
+++ public/scripts/admselectques.js
@@ -16,13 +16,20 @@ function init() {
 	* Param 5 - failure method - the method that is called in this js file when data fetch resulted in failure
 	*/
 	getFSQuestionList(null, 'addedOn', null, successQListFetch, null);
+	getPreviousGameQuesIds();
 }
 
+/**
+ * Success method of getFSQuestionList
+ * @param {*} querySnapshot - list of all questions from `questions` collection
+ */
 function successQListFetch(querySnapshot) {
 	// console.log("inside successQListFetch :: ");
 	let datajson = [];
 	let rowArr = [];
 	let innerjson = {};
+
+	// Iterate through all questions and prepare object for jquery datatable plugin
 	querySnapshot.forEach(function(doc) {
 		rowArr = [];
 		// console.log(doc.id, " => ", doc.data());
@@ -44,9 +51,12 @@ function successQListFetch(querySnapshot) {
 	});
 	let datajson1 = { data : datajson };
 	// console.log(JSON.stringify(datajson1));
+
+	// When HTML is all loaded ...
 	$(document).ready(function() {
 		var selected = [];
 
+		// ... apply data to jquery datatable plugin for displaying data on UI.
 		table = $('#questionList').DataTable( {
 			data: datajson,
 			columns: [
@@ -60,67 +70,20 @@ function successQListFetch(querySnapshot) {
 			"lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]]
 			
 		} );
-		// console.log(JSON.stringify(datajson));
 
+		// Track # of rows selected on the jquery datatable
 		$('#questionList tbody').on('click', 'tr', function () {
 			$(this).toggleClass('selected');
 			// console.log(table.rows('.selected').data());
 			$('#selectedCount').text(table.rows('.selected').data().length +' row(s) selected');
 		} );
 	} );
-
-	
-
-	// console.log('Returning from successQListFetch');
 }
 
 /**
- * method to create a new question in "questions" collection
+ * @returns nextGame date based on the year, month, date, hour, minute Select fields
  */
-/* function createQues() {
-	var ques;
-	var ans;
-	var pques;
-	var pans;
-	var info;
-	var status;
-	var keywords;
-	addToQuestionCollection(ques, ans, pques, pans, info, status, keywords, successCreateQues, failureCreateQues);
-} */
-/* function successCreateQues(doc) {
-	console.log('SuccessCreateQues :::');
-	console.log(doc.id);
-} */
-/* function failureCreateQues(error) {
-	console.log('FailureCreateQues :::' + error);
-} */
-
-/**
- * method to update a new question in "questions" collection
- */
-/* function updateQues() {
-	var quesDocId
-	var ques;
-	var ans;
-	var pques;
-	var pans;
-	var info;
-	var status;
-	var keywords;
-	updateQuestionInCollection(quesDocId, ques, ans, pques, pans, info, status, keywords, successUpdateQues, failureUpdateQues);
-} */
-/* function successUpdateQues(doc) {
-	console.log('SuccessUpdateQues :::');
-	console.log(doc.id);
-} */
-/* function failureUpdateQues(error) {
-	console.log('FailureUpdateQues :::' + error);
-} */
-
 function getNextGameDate() {
-    // console.log($('#inputNextGameYear').val());
-    // console.log($('#inputNextGameMonth').val());
-    // console.log($('#inputNextGameDate').val());
     nextGameDate 
 		= new Date(
 			$('#inputNextGameYear').val(), 
@@ -130,10 +93,13 @@ function getNextGameDate() {
             $('#inputNextGameMinute').val(), 
 			0
 		);
-    // console.log(nextGameDate);
 	return nextGameDate;
 }
 
+/**
+ * 
+ * @returns nextGameId based on the year, month, date component of nextGame date
+ */
 function getNextGameId() {
     nextGameDate = getNextGameDate();
     // console.log(nextGameDate);
@@ -142,7 +108,14 @@ function getNextGameId() {
     // d.setDate(d.getDate() + 10);
     return getCurrentGameFormattedDate(nextGameDate);
 }
+function getCurrentGameFormattedDate(dt) {
+    return '' + dt.getFullYear() + '' + (appendLeadingZeroes(dt.getMonth()+1)) + '' + appendLeadingZeroes(dt.getDate());
+}
 
+/**
+ * Validates if nextGame date is in future. If not, shows an alert.
+ * @returns 
+ */
 function isNextGameDateInFuture() {
     // console.log(getNextGameDate());
     // console.log(new Date());
@@ -153,20 +126,37 @@ function isNextGameDateInFuture() {
     return true;
 }
 
-function getCurrentGameFormattedDate(dt) {
-    return '' + dt.getFullYear() + '' + (appendLeadingZeroes(dt.getMonth()+1)) + '' + appendLeadingZeroes(dt.getDate());
-}
-
+/**
+ * Validates that 90 rows are selected in jquery datatable. If not, shows an alert.
+ * @returns 
+ */
 function ninetyRowsSelected() {
 	if (table.rows('.selected').data().length == 90) return true;
 	alert('90 questions are not yet selected.');
 	return false;
 }
 
+/**
+ * Validation on `future next date` and `90 questions selected` on button click
+ * @returns 
+ */
 function validateBeforeCreatingGame() {
 	return (isNextGameDateInFuture()  &&  ninetyRowsSelected());
 }
+/**
+ * Validation on `future next date` on button click. 
+ * This method is used when creating gameques from previous gameques. 
+ * Hence 90 question selection not required.
+ * @returns 
+ */
+function validateBeforeCreatingGameFromPrev() {
+	return (isNextGameDateInFuture());
+}
 
+/**
+ * Creates gameques based on selected questions from datatable.
+ * @returns 
+ */
 function createGameQues() {
 	if (!validateBeforeCreatingGame()) return;
 
@@ -196,51 +186,78 @@ function createGameQues() {
 	});
 }
 
+/**
+ * Button click handler - Select all rows
+ */
 function selectAllRows() {
 	$('#questionList tbody tr').addClass('selected');
 	$('#selectedCount').text(table.rows('.selected').data().length +' row(s) selected');
 }
-function clearAllRows() {
+/**
+ * Button click handler - Clear selection of rows
+ */
+ function clearAllRows() {
 	$('#questionList tbody tr').removeClass('selected');
 	$('#selectedCount').text(table.rows('.selected').data().length +' row(s) selected');
 }
 
 
-function createCurrGameSetting() {
+/**
+ * Create `currgame` Settings for gameques created from selected 90 questions. 
+ * This method validates that 90 questions should be selected and that nextgame is in future.
+ * @returns 
+ */
+ function createCurrGameSetting() {
 	if (!validateBeforeCreatingGame()) return;
 	getFSSettingsData(successFetchSettingsData, successFetchSettingsData); // For failure too call same function.
 }
 
+/**
+ * Create `currgame` Settings for gameques created from previous gameques. 
+ * This method removes the validation of selecting 90 questions.
+ * @returns 
+ */
+function createCurrGameSettingFromPrev() {
+	if (!validateBeforeCreatingGameFromPrev()) return;
+	getFSSettingsData(successFetchSettingsData, successFetchSettingsData); // For failure too call same function.
+}
+
+/**
+ * Success (& failure) method for createCurrGameSetting & createCurrGameSettingFromPrev
+ * @param {*} doc - currgame settings doc
+ */
 function successFetchSettingsData(doc) {
 	currGameSettings = doc.data();
 	// console.log('this.currGameSettings ::' + JSON.stringify(currGameSettings));
 	getFSCurrGameQuestions(getNextGameId(), successGameQuesFetch);
 }
 
+/**
+ * Success method for getFSCurrGameQuestions
+ * @param {*} doc - gameques doc based on getNextGameId()
+ */
 function successGameQuesFetch(doc) {
     var qList = doc.data();
-    // console.log(JSON.stringify(qList));
-    var answers = '';
+
+	var answers = '';
     Object.keys(qList).forEach((qdockey) => {
-        // console.log('qdockey ::' + qdockey);
         if (qdockey !== '_gameover') {
             answers += qdockey + '#';
         }
     });
-    // console.log('this.currGameSettings ::' + JSON.parse(JSON.stringify(currGameSettings)));
-    // console.log('answers ::' + answers);
-    db.collection("settings").doc("currgame").set({});
+
+	db.collection("settings").doc("currgame").set({});
     let settigs = db.collection("settings").doc("currgame");
     var settjson = {};
     settjson.answers = answers.substring(0, answers.length-1);
 
     var d = nextGameDate;
 	settjson.gamedatetime = firebase.firestore.Timestamp.fromDate(new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), 0, 0));
-	// console.log(getNextGameId());
-    settjson.gameid = getNextGameId();
+
+	settjson.gameid = getNextGameId();
     gameid = getFromStorage('gameid');
-    // console.log('gameid ::' + gameid);
-    settjson.prevgameid = gameid === undefined ? null : gameid;
+
+	settjson.prevgameid = gameid === undefined ? null : gameid;
     settjson.queschanged = firebase.firestore.Timestamp.now();
     settjson.gameover = false;
     settjson.coveredAnswers = null;
@@ -253,7 +270,7 @@ function successGameQuesFetch(doc) {
 		console.error('Error while setting existing properties from currgame', ex);
 	}
 
-    // console.log('settjson ::' + JSON.stringify(settjson));
+    // Update currgame settings
     settigs.update(settjson, { merge: true }).then(() => {
         $('#messageText').text('Settings created successfully');
     })
@@ -262,6 +279,7 @@ function successGameQuesFetch(doc) {
         $('#messageText').text('Failed creating Settings');
     });
 
+	// Reset latest entry in prizes collection
 	console.log('Starting to reset Prizes');
 	db.collection("prizes").doc("latest").set({})
 	.then(() => {
@@ -273,7 +291,83 @@ function successGameQuesFetch(doc) {
     });
 }
 
+/**
+ * Retrieve list of all previous gameques ids. This is used to display entries in Select input.
+ */
+function getPreviousGameQuesIds() {
+	getFirestoreDataColl('gameques', null, null, null, successGetPrevGameQuesIds, failureGetPrevGameQuesIds);
+}
 
+/**
+ * Success method for retrieving all gameques ids. Populates Select input field.
+ * @param {*} gameIds - list of all gameques ids
+ */
+function successGetPrevGameQuesIds(gameIds) {
+	console.log('successGetPrevGameQuesIds');
+	var sel = $('#prevGameQuesSelect');
+	gameIds.forEach(function(doc) {
+		console.log(doc.id);
+		console.log(JSON.stringify(doc.id));
+		sel.append($("<option>").attr('value',doc.id).text(doc.id));
+	});
+}
+function failureGetPrevGameQuesIds(err) {
+	console.log('failureGetPrevGameQuesIds');
+	console.log(JSON.stringify(err));
+}
+
+/**
+ * Creates a new gameques doc based on the select previous gameques (Select input field)
+ */
+function setPrevGameAsCurrGame() {
+	console.log($('#prevGameQuesSelect').val());
+	getFirestoreData('gameques', $('#prevGameQuesSelect').val(), successGetPrevGameQues, failureGetPrevGameQues);
+}
+/**
+ * 
+ * @param {*} doc - previous gameques doc based on selected gameques id
+ */
+function successGetPrevGameQues(doc) {
+	// Retrieve next game id
+	nextGameId = getNextGameId();
+	console.log('nextGameId ::' + nextGameId);
+
+	// Iterate through prev gameques doc and create new gameques doc
+	let quesList = {};
+	Object.keys(doc.data()).forEach((qdockey) => {
+		let qdocPrev = doc.data()[qdockey];
+		let qdoc = {};
+		qdoc.question = qdocPrev.question;
+		qdoc.answer = qdocPrev.answer;
+		qdoc.info = qdocPrev.info;
+		if (qdocPrev.new) qdoc.new = qdocPrev.new;
+		quesList[qdoc.answer] = qdoc;
+	});
+	console.log(quesList);
+	console.log(JSON.stringify(quesList));
+
+	// Create a new entry in gameques based on nextGameId
+	db.collection("gameques").doc(nextGameId).set({});
+	let gameques = db.collection("gameques").doc(nextGameId);
+
+	// Set new gameques doc for the newly created gameques record
+	gameques.update(quesList, { merge: true })
+	.then(() => {
+		$('#messageText').text('Game questions created successfully');
+	})
+	.catch(function(error) {
+		console.error("Error adding document: ", error);
+		$('#messageText').text('Failed creating Game questions');
+	});
+}
+function failureGetPrevGameQues(err) {
+	console.log('failureGetPrevGameQues');
+	console.log(JSON.stringify(err));
+}
+
+/**
+ * On document load
+ */
 $(function onDocReady() {
 	// console.log('Inside onDocReady');
     loadHeaderActionsAdmin();
@@ -281,9 +375,11 @@ $(function onDocReady() {
     $('#btnLogout').click(signout);
     $('#createGameQues').click(createGameQues);
     $('#setNextCurrGame').click(createCurrGameSetting);
-    $('#setNextCurrGame').click(createCurrGameSetting);
+    $('#setNextCurrGameFromPrev').click(createCurrGameSettingFromPrev);
+	
     $('#selectAll').click(selectAllRows);
     $('#clearAll').click(clearAllRows);
+	$('#setPrevGameAsCurrGame').click(setPrevGameAsCurrGame);
 });
 
 
