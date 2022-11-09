f54efc6f1cfdcf3f1bf88cbca0141186aeeec5e0
hpreets <mrharpreets@gmail.com>
2022-11-09 11:33:16 -0800

    adding analytics

--- public/scripts/init-firebase.js
+++ public/scripts/init-firebase.js
@@ -5,8 +5,11 @@ const firebaseConfig = {
     projectId: "sikhitambola",
     storageBucket: "sikhitambola.appspot.com",
     messagingSenderId: "520012792742",
-    appId: "1:520012792742:web:790a81900e73845bf98567"
+    appId: "1:520012792742:web:790a81900e73845bf98567",
+	measurementId: "G-4K4F0EMPSG"
   };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const stApp = firebase.initializeApp(firebaseConfig);
+const analytics = firebase.analytics(stApp);
+
